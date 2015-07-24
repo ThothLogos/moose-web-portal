bd8bca843359a1cfcd5e296aa2f6acda623c0c01
ThothLogos <naughton.joshua@gmail.com>
2015-07-24 12:19:08 -0400

    Refactor

--- data.rb
+++ data.rb
@@ -9,11 +9,9 @@ class UserData
                            :dbname => db_name,
                            :user => db_user,
                            :password => db_password)
-    
     # ~ Debug ~ #
     res = @conn.exec("SELECT * FROM users")
     fields = res.fields
-
     res.each do |row|
       puts row["id"] + " " + row["username"] + " " + row["email"] + " " +
            row["password"] + " " + row["salt"] + " " + row["created"]
@@ -24,14 +22,14 @@ class UserData
   def username_exist?(username)
     res = @conn.exec("SELECT username FROM users WHERE username = '#{username}'")
     # If the query is empty the username doesn't exist, return false
-    res.field_values("username").empty? ? (return false) : (return true)
+    return res.field_values("username").empty? ? false : true
   end
 
   # Query user table for already registered email
   def email_exist?(email)
     res = @conn.exec("SELECT email FROM users WHERE email='#{email}'")
     # If the query is empty the email doesn't exist, return false
-    res.field_values("email").empty? ? (return false) : (return true)
+    return res.field_values("email").empty? ? false : true
   end
 
   # Add new user to users table
@@ -41,7 +39,7 @@ class UserData
                         '#{email}',
                         '#{password}',
                         '#{salt}',
-                        '#{DateTime.now}')   ")
+                        '#{DateTime.now}' ) ")
   end
 
 end
