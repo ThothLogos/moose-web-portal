ed936f0b8d27b224b46fdd0a0034e87dff36c19a
ThothLogos <naughton.joshua@gmail.com>
2015-07-26 18:26:07 -0400

    Add getters/setters/checkers for password reset code, add s end_mail_recovery to begin password reset process

--- data.rb
+++ data.rb
@@ -43,11 +43,11 @@ class UserData
                         '#{salt}',
                         '#{DateTime.now}',
                         'f',
-                        '#{generate_verification_code}' ) ")
+                        '#{generate_random_code}' ) ")
   end
 
-  # Random 12-character code to serve as email verification/account activation
-  def generate_verification_code
+  # Random 16-character code to serve as email verification/password reset
+  def generate_random_code
     return SecureRandom.hex(8)
   end
 
@@ -57,6 +57,7 @@ class UserData
     return res.getvalue(0,0)
   end
 
+  # Checks to see if the provided verification code exists in the database
   def verification_exist?(code)
     res = @conn.exec("SELECT verifycode FROM users WHERE verifycode='#{code}'")
     return res.field_values("verifycode").empty? ? false : true
@@ -65,6 +66,9 @@ class UserData
   # Packages and sends email verification via SMTP
   def send_mail_verification(email, server, port, username, password)
     subject = "Account Verification"
+    body = ["Thank you for registering. You will need to verify this email ",
+            "before your account is active.\n\n\n",
+            "Verification Code: #{verification_code(email)}"]
     Pony.mail(
       to:                   email,
       via:                  :smtp,
@@ -78,15 +82,53 @@ class UserData
         domain:                 "localhost"
                             },
       subject:              subject,
-      body:                 ("Thank you for registering. You'll need to verify this"
-                          + " email before your account is active.\n\n\n"
-                          + "Verification code: #{verification_code(email)}") )
+      body:                 body.join )
   end
 
   # Change account verified flag to true based on verification code
   def verify_account(code)
     res = @conn.exec("SELECT id FROM users WHERE verifycode='#{code}'")
     @conn.exec("UPDATE users SET verified='t' WHERE id='#{res.getvalue(0,0)}'")
+  end
+
+  # Inserts a new random code into the database to be used for password reset
+  def set_reset_code(email)
+    @conn.exec("UPDATE users SET resetcode='#{generate_random_code}'
+                WHERE email='#{email}'")
+  end
+
+  # Returns the stored reset code for a provided email
+  def reset_code(email)
+    res = @conn.exec("SELECT resetcode FROM users WHERE email='#{email}'")
+    return res.getvalue(0,0)
+  end
+
+  # Checks to see if a provided password reset code exists in the database
+  def reset_exist?(code)
+    res = @conn.exec("SELECT resetcode FROM users WHERE resetcode='#{code}'")
+    return res.field_values("resetcode").empty? ? false : true
+  end
+
+  # Packages and sends password recovery request email via SMTP
+  def send_mail_recovery(email, server, port, username, password)
+    subject = "Password Reset Request"
+    body = ["A password reset request has been initiated for this account. ",
+            "If you did not initiate this request, please ignore this message. ",
+            "\n\n\nAccount Recovery Code: #{reset_code(email)}"]
+    Pony.mail(
+      to:                   email,
+      via:                  :smtp,
+      via_options:          {
+        address:                server,
+        port:                   port,
+        enable_starttls_auto:   true,
+        user_name:              username,
+        password:               password,
+        authentication:         :plain,
+        domain:                 "localhost"
+                            },
+      subject:              subject,
+      body:                 body.join )
   end
 
 end
